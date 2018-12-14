ff39513a198fd31bc3c11c266b853a4bc0a3df4a
luoluo123a <45871685+luoluo123a@users.noreply.github.com>
2018-12-14 21:07:47 +0800

    Update SpringBootMapperApplication.java

--- src/main/java/com/baizhi/lpm/SpringBootMapperApplication.java
+++ src/main/java/com/baizhi/lpm/SpringBootMapperApplication.java
@@ -7,6 +7,11 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.baizhi.lpm.mapper")
 @SpringBootApplication
 public class SpringBootMapperApplication {
+    
+    
+    
+    
+    
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMapperApplication.class, args);
