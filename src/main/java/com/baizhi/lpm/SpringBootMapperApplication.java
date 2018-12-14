9fcf59e8b5068b2750d8e51ffd63967b95d23135
lpm <1334322170@qq.com>
2018-12-14 21:14:43 +0800

    v2.0

--- src/main/java/com/baizhi/lpm/SpringBootMapperApplication.java
+++ src/main/java/com/baizhi/lpm/SpringBootMapperApplication.java
@@ -7,12 +7,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.baizhi.lpm.mapper")
 @SpringBootApplication
 public class SpringBootMapperApplication {
-    
-    
-    
-    
-    
-
+ 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMapperApplication.class, args);
     }
